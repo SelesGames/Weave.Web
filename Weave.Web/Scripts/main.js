f5e36791841215c58544acfe1c80479df84f984f
ericsarvey <ericsarvey@gmail.com>
2014-01-24 17:24:32 -0800

    almost scrolling and loading data

--- Weave.Web/Scripts/main.js
+++ Weave.Web/Scripts/main.js
@@ -14,17 +14,21 @@ var ds = {
                 log(err);
             });
     },
+    lastNewsArg: null,
     getUserNews: function (categoryId, entry, skip, take, type, req, success) {
+
+        this.lastNewsArg = {
+            userId: ds.userId,
+            category: categoryId,
+            entry: entry,
+            skip: skip,
+            take: take,
+            type: type,
+            requireImage: req
+        };
+
         this.get('/user/news',
-            {
-                userId: ds.userId,
-                category: categoryId,
-                entry: entry,
-                skip: skip,
-                take: take,
-                type: type,
-                requireImage: req
-            },
+            this.lastNewsArg,
             success,
             function (err) {
                 log('getUserNews error' + err.statusText);
@@ -32,16 +36,19 @@ var ds = {
             });
     },
     getUserNewsByFeedId: function (feedId, entry, skip, take, type, req, success) {
+
+        this.lastNewsArg = {
+            userId: ds.userId,
+            FeedId: feedId,
+            entry: entry,
+            skip: skip,
+            take: take,
+            type: type,
+            requireImage: req
+        };
+
         this.get('/user/news',
-            {
-                userId: ds.userId,
-                FeedId: feedId,
-                entry: entry,
-                skip: skip,
-                take: take,
-                type: type,
-                requireImage: req
-            },
+            this.lastNewsArg,
             success,
             function (err) {
                 log('getUserNews error' + err.statusText);
@@ -117,13 +124,18 @@ function getFeeds(userInfo, category) {
     return feedVMs;
 }
 
+var modes = { all: 'all', category: 'category', feed: 'feed' };
+
 /* knockoutjs */
 var vm = {
     categories: ko.observableArray(),
     // latest, cat, or feed specific
     news: ko.observableArray(),
-    getNewsForCategory: function(categoryVM){
-        ds.getUserNews(categoryVM.Name, 'Mark', 0, 10, 1, false,
+    mode: modes.category,
+    getNewsForCategory: function (categoryVM) {
+        this.mode = modes.category;
+        //24 12 on screen 12 off
+        ds.getUserNews(categoryVM.Name, 'Mark', 0, 24, 1, false,
         function (data) {
             log('getUserNews for category ' + categoryVM.Name);
             log(data);
@@ -141,8 +153,8 @@ var vm = {
         });
     },
     getUserNewsByFeedId: function (feed) {
-        
-        ds.getUserNewsByFeedId(feed.Id, 'Mark', 0, 10, 1, false,
+        this.mode = modes.feed;
+        ds.getUserNewsByFeedId(feed.Id, 'Mark', 0, 24, 1, false,
         function (data) {
             log('getNewsByFeed Id success' + feed.Id);
             log(data);
@@ -166,8 +178,31 @@ var vm = {
     scrollRemoveNews: function (news) {
         this.news.remove(news);
     },
-    scrollAppendNews: function(news){
-        this.news.push(news);
+    scrollAppendNews: function () {
+        // load 12 more
+        if(vm.mode == modes.category){
+            ds.getUserNews(ds.lastNewsArg.category, ds.lastNewsArg.entry, ds.lastNewsArg.skip + ds.lastNewsArg.take, ds.lastNewsArg.take, ds.lastNewsArg.type,
+            function (data) {
+                log('scrollAppendNews');
+                log(data);
+                //for (var n = 0, nlen = data.News.length; n < nlen; n++) {
+                //    var news = data.News[n];
+                //    var newsVM = {
+                //        Id: news.Id,
+                //        Title: news.Title,
+                //        ImageUrl: news.ImageUrl,
+                //        HasImage: typeof news.ImageUrl != 'undefined'
+                //    };
+                //    this.news.push(newsVM);
+                //}
+            });
+        }
+        else if (this.mode == modes.feed) {
+
+        }
+        else if (this.mode == modes.all) {
+
+        }
     }
 }
 
@@ -213,6 +248,54 @@ $(function () {
 
         //log('userInfo ' + (new Date().getTime() - s));
         log(userInfo);
+    });
+
+    
+    var lastY = 0;
+    var lastLoadPositiveY = 0;
+    var skip = 0;
+    var take = 12;
+    var total = 0;
+    $('#content').scroll(function(evt) {
+
+        var height = window.innerHeight;
+        var curY = $(this).scrollTop();
+        var DOWN = 'down', UP = 'up';
+        var direction = curY > lastY ? DOWN : UP;
+        lastY = curY;
+        
+        var contentDelta = curY - window.innerHeight;
+        var loadPosDelta = height / 4;
+        var bLoadPositive = contentDelta > loadPosDelta;
+
+        log('vm.mode ' + vm.mode + ' content scrolled total height ' + height + ' scrollTop() ' + curY + ' direction ' + direction
+            + ' contentDelta ' + contentDelta + ' bLoadPositive ' + bLoadPositive + ' loadPosDelta ' + loadPosDelta);
+
+        if (bLoadPositive) {
+            if (lastLoadPositiveY >= 3 * loadPosDelta) {
+                lastLoadPositiveY = 0;
+                log('load positive');
+
+                // remove the first 12
+                vm.news.splice(0, 11);
+                // append new 12
+                vm.scrollAppendNews();
+
+                skip += total;
+            }
+            else {
+                log('load positive skipped');
+                lastLoadPositiveY += loadPosDelta;
+            }
+        }
+        else {
+            // TODO delta logic
+            // if direction
+            // fetch previous 12
+            // remove/splice last 12
+        }
+      
+    
     });
 
 
